d483b22dde1fe8de71c0b027d701d5bf7524326e
oesp <riyazulislam199@gmail.com>
2024-10-20 14:09:29 +0530

    third commit

--- student-enrollment-api/server.js
+++ student-enrollment-api/server.js
@@ -25,6 +25,10 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api', studentRoutes);
 
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
